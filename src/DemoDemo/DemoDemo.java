9dbd55aa75d05579131906e143f244d3ea32097d
WU Jitao <1191289704@qq.com>
2022-02-21 11:04:52 +0800

    new files

--- src/DemoDemo/DemoDemo.java
+++ src/DemoDemo/DemoDemo.java
@@ -1,30 +1,50 @@
 package DemoDemo;
 
 
+import com.sun.source.tree.BreakTree;
+
 import java.util.*;
 
 public class DemoDemo {
     public static void main(String[] args) {
-        int[] nums1 = {2, 4, 1, 5, 5};
-        int[] nums2 = {2, 2, 5, 5, 8};
-        List<Integer> integers = new ArrayList<>();
-        Map<Integer, Integer> integerIntegerMap = new HashMap<>();
-        for (int num : nums1) {
-            if (integerIntegerMap.containsKey(num)) {
-                integerIntegerMap.put(num, integerIntegerMap.get(num) + 1);
-            } else integerIntegerMap.put(num, 1);
+        int[] digits = {1,4,3,6,4,9,1};
+        enum Cases {
+            CARRY, NOCARRY, NORMAL;
         }
-        for (int num : nums2) {
-            if (integerIntegerMap.containsKey(num)) {
-                if (integerIntegerMap.get(num) > 0) {
-                    integers.add(num);
-                    integerIntegerMap.put(num, integerIntegerMap.get(num) - 1);
-                }
+        Cases cases = Cases.NORMAL;
+        int pointer = digits.length - 1;
+        while(digits[pointer] == 9){
+            if(pointer == 0){
+                cases = Cases.CARRY;
+                digits[pointer] = 0;
+            }
+            else{
+                cases = Cases.NOCARRY;
+                digits[pointer] = 0;
+                pointer--;
             }
         }
-        System.out.println(integers);
-
-
+        if(cases == Cases.NOCARRY){
+            digits[pointer] ++;
+        }
+        if(cases == Cases.NORMAL){
+            digits[digits.length-1] ++;
+        }
+        int [] ret;
+        if(cases == Cases.CARRY){
+            ret = new int[digits.length + 1];
+            ret[0] = 1;
+            for(int i = 1; i < digits.length + 1; i++){
+                ret[i] = 0;
+            }
+        }
+        else{
+            ret = new int[digits.length];
+            for(int i = 0; i < digits.length; i++) {
+                ret[i] = digits[i];
+            }
+        }
+        System.out.println(Arrays.toString(ret));
 //code ends here
     }
 
